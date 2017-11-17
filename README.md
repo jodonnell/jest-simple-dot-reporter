c0ce2bd7f2dd7de8d7da10d334530677da4c794e
Jacob O'Donnell <jacobodonnell@gmail.com>
2017-11-16 22:39:44 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,27 @@
+# Simple dot reporter for jest
+
+No dependencies, no interactive shell needed, just dots.  Also this is nice because it won't swallow your console.logs.
+
+## Installation
+
+You may install this package as a development dependency:
+
+```bash
+npm install --save-dev jest-simple-dot-reporter
+yarn add --dev jest-simple-dot-reporter
+```
+
+## Configuration
+
+Configure [Jest](https://facebook.github.io/jest/docs/en/configuration.html) to use the reporter.
+
+For example, create a `jest.config.js` file containing:
+
+```javascript
+module.exports = {
+  verbose: false,
+  reporters: [
+    'jest-simple-dot-reporter'
+  ]
+};
+```
---
+++ jest-simple-dot-reporter.js
@@ -0,0 +1,49 @@
+class JestSimpleDotReporter {
+    constructor(globalConfig, options) {
+        this._globalConfig = globalConfig;
+        this._options = options;
+    }
+
+    onRunStart(test) {
+        console.log();
+        console.log(`Found ${test.numTotalTestSuites} test suites`);
+    }
+
+    onRunComplete(test, results) {
+        const {
+            numFailedTests,
+            numPassedTests,
+            numPendingTests,
+            testResults,
+            numTotalTests,
+            startTime
+        } = results;
+
+        console.log();
+        testResults.map(({failureMessage}) => {
+            if (failureMessage) {
+                console.log(failureMessage);
+            }
+        });
+
+        console.log(`Ran ${numTotalTests} tests in ${testDuration()}`);
+        process.stdout.write(` ${numPassedTests || 0} passing`);
+        process.stdout.write(` ${numFailedTests || 0} failing`);
+        process.stdout.write(` ${numPendingTests || 0} pending`);
+        console.log();
+
+        function testDuration() {
+            const end = new Date();
+            const start = new Date(startTime);
+
+            const seconds = (end - start) / 1000;
+            return `${seconds} s`;
+        }
+    }
+
+    onTestResult() {
+        process.stdout.write('.');
+    }
+}
+
+module.exports = JestSimpleDotReporter;
